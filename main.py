1b86c5458e9685cbcb1def403f446fc79b7ee709
R.SABARISH RAJA <80580833+sabarishraja@users.noreply.github.com>
2022-02-28 22:52:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-
-done()
+import turtle as t
+o = t.Turtle()
+num_of_sides = 8
+for  po in range(num_of_sides):
+    for _ in range(num_of_sides):
+        angle = 360 / num_of_sides
+        o.forward(100)
+        o.right(angle)
+    num_of_sides-=1
